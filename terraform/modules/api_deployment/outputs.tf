70094e5551e0d387dd6b5c32ce21a3392120e207
john-matlock-eng <john@matlock.engineering>
2024-07-06 16:04:10 -0500

    comment it out

--- terraform/modules/api_deployment/outputs.tf
+++ terraform/modules/api_deployment/outputs.tf
@@ -1,4 +1,4 @@
-output "api_gateway_url" {
-  description = "URL of the deployed API Gateway"
-  value       = module.api_deployment.api_gateway_url
-}
+# output "api_gateway_url" {
+#   description = "URL of the deployed API Gateway"
+#   value       = module.api_deployment.api_gateway_url
+# }
