aaa6cf85937d03fcd9ce7a9dd3c96fcaf8008bbc
john-matlock-eng <john@matlock.engineering>
2024-07-07 11:37:58 -0500

    more workflow changes

--- terraform/apis/ecr/main.tf
+++ terraform/apis/ecr/main.tf
@@ -9,3 +9,24 @@ resource "aws_ecr_repository" "repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 3 images"
+        selection    = {
+          tagStatus    = "any"
+          countType    = "imageCountMoreThan"
+          countNumber  = 3
+        }
+        action       = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
