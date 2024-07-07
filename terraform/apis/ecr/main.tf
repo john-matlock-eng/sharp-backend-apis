af6ff55ef24ea2b2e5205030baa60165b4d217b5
john-matlock-eng <john@matlock.engineering>
2024-07-07 12:27:55 -0500

    Update main.tf

--- terraform/apis/ecr/main.tf
+++ terraform/apis/ecr/main.tf
@@ -2,12 +2,6 @@
 resource "aws_ecr_repository" "repo" {
   name                 = var.api_name
   image_tag_mutability = "IMMUTABLE"
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }
 
 resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
