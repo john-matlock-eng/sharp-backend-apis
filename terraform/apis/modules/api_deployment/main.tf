fedf1ac86f045af09dc3dc3cd65e4c400ee0f5e8
john-matlock-eng <john@matlock.engineering>
2024-07-07 21:24:05 -0500

    Update main.tf

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -6,12 +6,16 @@ data "aws_ecr_repository" "api_ecr_repository" {
   name = "${var.api_name}"
 }
 
+data "aws_iam_role" "api_lambda_exec" {
+  name = "api_lambda_exec"
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name = var.api_name
   image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
   handler       = var.lambda_handler
   runtime       = var.runtime
-  role          = aws_iam_role.lambda_exec.arn
+  role          = data.aws_iam_role.api_lambda_exec.arn
 }
 
 # # API Gateway
