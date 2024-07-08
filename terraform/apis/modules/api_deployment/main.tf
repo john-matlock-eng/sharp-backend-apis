0d0418df179c262fe375d7221f65a0027c1733b0
john-matlock-eng <john@matlock.engineering>
2024-07-08 16:42:38 -0500

    Update main.tf

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -12,6 +12,7 @@ data "aws_iam_role" "api_lambda_exec" {
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.api_name
+  package_type = "Image"
   image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
   role          = data.aws_iam_role.api_lambda_exec.arn
   architectures = ["arm64"]
