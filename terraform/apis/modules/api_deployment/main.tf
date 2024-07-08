8ad071742c3a49a34f8dfd6a0ddd4063656468dc
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-07-07 21:34:29 -0500

    Lambda

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -2,56 +2,21 @@ provider "aws" {
   region = var.aws_region
 }
 
-# # ECR Repository
-# resource "aws_ecr_repository" "repo" {
-#   name                 = var.api_name
-#   image_tag_mutability = "IMMUTABLE"
-#   encryption_configuration {
-#     encryption_type = "AES256"
-#   }
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+data "aws_ecr_repository" "api_ecr_repository" {
+  name = "${var.api_name}"
+}
 
+data "aws_iam_role" "api_lambda_exec" {
+  name = "api_lambda_exec"
+}
 
-
-# # Lambda function
-# resource "aws_lambda_function" "lambda" {
-#   function_name = var.api_name
-#   image_uri     = "${aws_ecr_repository.repo.repository_url}:${var.image_tag}"
-#   handler       = var.lambda_handler
-#   runtime       = var.runtime
-#   role          = aws_iam_role.lambda_exec.arn
-# }
-
-# # API Gateway
-# resource "aws_api_gateway_rest_api" "api" {
-#   name = var.api_name
-# }
-
-# resource "aws_api_gateway_resource" "resource" {
-#   rest_api_id = aws_api_gateway_rest_api.api.id
-#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-#   path_part   = "users"
-# }
-
-# resource "aws_api_gateway_method" "method" {
-#   rest_api_id   = aws_api_gateway_rest_api.api.id
-#   resource_id   = aws_api_gateway_resource.resource.id
-#   http_method   = "GET"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "integration" {
-#   rest_api_id             = aws_api_gateway_rest_api.api.id
-#   resource_id             = aws_api_gateway_resource.resource.id
-#   http_method             = aws_api_gateway_method.method.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.lambda.invoke_arn
-# }
-
-# output "api_gateway_url" {
-#   value = aws_api_gateway_rest_api.api.execution_arn
-# }
+resource "aws_lambda_function" "lambda" {
+  function_name = var.api_name
+  image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
+  handler       = var.lambda_handler
+  runtime       = var.runtime
+  role          = data.aws_iam_role.api_lambda_exec.arn
+  architectures = ["arm64"]
+  memory_size   = var.memory_size
+  timeout       = var.timeout
+}
