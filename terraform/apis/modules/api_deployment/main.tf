bc27ad10fe2aa416e7d73c06db88a4885bd1da97
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-07-09 11:58:02 -0500

    Ditch ARM for now

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "lambda" {
   package_type = "Image"
   image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
   role          = data.aws_iam_role.api_lambda_exec.arn
-  architectures = ["arm64"]
+  architectures = ["x86_64"]
   memory_size   = var.memory_size
   timeout       = var.timeout
 }
