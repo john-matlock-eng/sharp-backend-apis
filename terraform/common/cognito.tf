ff6281477dccc6c3df7bc9d2401dd52316f7078c
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-07-12 20:16:16 -0500

    Update cognito.tf (#40)

--- terraform/common/cognito.tf
+++ terraform/common/cognito.tf
@@ -41,7 +41,7 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
   user_pool_id                 = aws_cognito_user_pool.user_pool.id
   name                         = "sharp_user_pool_client"
   generate_secret              = false
-  explicit_auth_flows          = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  explicit_auth_flows          = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
   supported_identity_providers = ["COGNITO"]
   allowed_oauth_flows          = ["code"]
   allowed_oauth_scopes         = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
