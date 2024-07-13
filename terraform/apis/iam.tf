1d8471da75af5fe226b1ac834ebc3e61538ae468
john-matlock-eng <john@matlock.engineering>
2024-07-13 15:14:34 -0500

    Update iam.tf

--- terraform/apis/iam.tf
+++ terraform/apis/iam.tf
@@ -23,6 +23,6 @@ data "aws_iam_policy" "lambda_exec" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
+  role       = data.aws_iam_policy.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
