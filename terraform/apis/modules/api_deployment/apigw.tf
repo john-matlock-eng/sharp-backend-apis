95727fcc9b15038b1688f7f45ebc76b53ffb4df1
john-matlock-eng <john@matlock.engineering>
2024-07-13 18:00:22 -0500

    Update apigw.tf

--- terraform/apis/modules/api_deployment/apigw.tf
+++ terraform/apis/modules/api_deployment/apigw.tf
@@ -9,40 +9,33 @@ resource "aws_api_gateway_resource" "resource" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "any_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  http_method             = aws_api_gateway_method.any_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on  = [aws_api_gateway_integration.lambda]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
 }
 
-resource "aws_lambda_permission" "api_gw_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_method_response" "method_response" {
+resource "aws_api_gateway_method_response" "any_method_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
+  http_method = aws_api_gateway_method.any_method.http_method
   status_code = "200"
 
   response_models = {
@@ -50,11 +43,11 @@ resource "aws_api_gateway_method_response" "method_response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "integration_response" {
+resource "aws_api_gateway_integration_response" "any_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.method_response.status_code
+  http_method = aws_api_gateway_method.any_method.http_method
+  status_code = aws_api_gateway_method_response.any_method_response.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
@@ -70,7 +63,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
 resource "aws_api_gateway_method_response" "options_method_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = "200"
 
   response_parameters = {
@@ -87,7 +80,7 @@ resource "aws_api_gateway_method_response" "options_method_response" {
 resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.options_method.http_method
   status_code = aws_api_gateway_method_response.options_method_response.status_code
 
   response_parameters = {
@@ -99,4 +92,45 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   response_templates = {
     "application/json" = ""
   }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_method_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.options_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
+
+resource "aws_lambda_permission" "api_gw_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
