cfbda7b20086bbf0dc4e189fd90a187d065d7bfe
john-matlock-eng <john@matlock.engineering>
2024-07-13 18:02:02 -0500

    Update apigw.tf

--- terraform/apis/modules/api_deployment/apigw.tf
+++ terraform/apis/modules/api_deployment/apigw.tf
@@ -101,23 +101,6 @@ resource "aws_api_gateway_integration" "options_integration" {
   type        = "MOCK"
 }
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = aws_api_gateway_method_response.options_method_response.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
