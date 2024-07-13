fbdde00809531081141ea9c9592f12618caff893
john-matlock-eng <john@matlock.engineering>
2024-07-13 15:55:56 -0500

    Update main.tf

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -6,15 +6,36 @@ data "aws_ecr_repository" "api_ecr_repository" {
   name = var.api_name
 }
 
-data "aws_iam_role" "api_lambda_exec" {
-  name = "${var.api_name}_api_lambda_exec"
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "${var.api_name}_lambda_exec"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_policy_attachment" "api_lambda_policy_attachment" {
+  name       = "cognito_post_confirmation_lambda_exec_attachment"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.api_name
   package_type  = "Image"
   image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
-  role          = data.aws_iam_role.api_lambda_exec.arn
+  role          = aws_iam_role.lambda_exec_role.arn
   architectures = ["x86_64"]
   memory_size   = var.memory_size
   timeout       = var.timeout
