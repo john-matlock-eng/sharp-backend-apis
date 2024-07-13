f69f695086248b7a49f047cc85c5e7e76a25250f
john-matlock-eng <john@matlock.engineering>
2024-07-13 15:51:14 -0500

    Update main.tf

--- terraform/apis/modules/api_deployment/main.tf
+++ terraform/apis/modules/api_deployment/main.tf
@@ -6,15 +6,12 @@ data "aws_ecr_repository" "api_ecr_repository" {
   name = var.api_name
 }
 
-data "aws_iam_role" "api_lambda_exec" {
-  name = "${var.api_name}_api_lambda_exec"
-}
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.api_name
   package_type  = "Image"
   image_uri     = "${data.aws_ecr_repository.api_ecr_repository.repository_url}:${var.image_tag}"
-  role          = data.aws_iam_role.api_lambda_exec.arn
+  role          = aws_iam_role.api_lambda_exec.arn
   architectures = ["x86_64"]
   memory_size   = var.memory_size
   timeout       = var.timeout
