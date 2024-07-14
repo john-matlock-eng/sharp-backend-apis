8139090df9009e3ac541c92ca4c8dad1e9319edd
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-07-14 10:53:53 -0500

    Update apigw.tf (#56)

--- terraform/apis/modules/api_deployment/apigw.tf
+++ terraform/apis/modules/api_deployment/apigw.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "${var.api_name}/{proxy+}"
+  path_part   = "{proxy+}"
 }
 
 # ANY method
