ec5fe358b417855c18cd1b4610d09e302c3d49e6
john-matlock-eng <john@matlock.engineering>
2024-07-14 19:31:48 -0500

    fix reference to variable

--- terraform/apis/apis.tf
+++ terraform/apis/apis.tf
@@ -1,10 +1,14 @@
 
 module "api_deployment" {
-  source          = "./modules/api_deployment"
-  aws_region      = var.aws_region
-  api_name        = var.api_name
-  lambda_handler  = "app.main.handler"
-  runtime         = "python3.12"
-  image_tag       = var.image_tag
-  build_platform  = "linux/arm64"
+  source                      = "./modules/api_deployment"
+  aws_region                  = var.aws_region
+  api_name                    = var.api_name
+  lambda_handler              = "app.main.handler"
+  runtime                     = "python3.12"
+  image_tag                   = var.image_tag
+  build_platform              = "linux/arm64"
+  memory_size                 = 256
+  timeout                     = 30
+  cognito_user_pool_id        = var.cognito_user_pool_id
+  cognito_user_pool_client_id = var.cognito_user_pool_client_id
 }
