83a93a91347275b724bad15050a77d7fd12332fe
john-matlock-eng <john@matlock.engineering>
2024-07-20 17:52:49 -0500

    Update post_confirmation_handler.py

--- cognito_post_confirmation/app/post_confirmation_handler.py
+++ cognito_post_confirmation/app/post_confirmation_handler.py
@@ -1,15 +1,16 @@
 import os
 from services.user_service import UserService
 from lib.dynamodb_controller import DynamoDBController
+from models.user_schema import UserCreate
 
 def handler(event, context):
     user_sub = event['request']['userAttributes']['sub']
     moniker = event['request']['userAttributes'].get('custom:moniker')
-    
+    user: UserCreate = UserCreate(user_id=user_sub, moniker=moniker)
     table_name = os.getenv('TABLE_NAME', 'sharp_app_data')
     dynamodb_controller = DynamoDBController(table_name)
     user_service = UserService(dynamodb_controller)
     
-    user_service.create_user(user_sub, moniker)
+    user_service.create_user(user)
     
     return event
