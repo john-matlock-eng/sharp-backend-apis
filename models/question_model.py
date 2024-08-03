e011c7e3665465aa862e05c57209ac3a8d9465be
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-08-03 11:47:24 -0500

    Create question_model.py

---
+++ models/question_model.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, UUID4
+from typing import List, Optional
+
+class QuestionBase(BaseModel):
+    question_text: str
+    options: List[str]
+    answer: List[str]
+
+class QuestionCreate(QuestionBase):
+    question_id: UUID4
+    quiz_id: UUID4
+    community_id: UUID4
+
+class QuestionUpdate(BaseModel):
+    question_text: Optional[str] = None
+    options: Optional[List[str]] = None
+    answer: Optional[List[str]] = None
+
+class QuestionInDB(QuestionBase):
+    question_id: UUID4
+    quiz_id: UUID4
+    community_id: UUID4
