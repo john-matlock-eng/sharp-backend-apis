06ec726bd8340f199e7fee40a9922810a55d4cfa
john-matlock-eng <john@matlock.engineering>
2024-08-29 20:25:25 -0500

    Update iam.tf

--- lambdas/web_scraper/terraform/iam.tf
+++ lambdas/web_scraper/terraform/iam.tf
@@ -3,7 +3,7 @@ data "aws_sqs_queue" "knowledge_source_processing_queue" {
 }
 
 resource "aws_lambda_permission" "allow_sqs_trigger" {
-  statement_id  = "AllowSQSTrigger"
+  statement_id  = "AllowSQSTrigger_web_scraper"
   action        = "lambda:InvokeFunction"
   function_name = "web_scraper"
   principal     = "sqs.amazonaws.com"
